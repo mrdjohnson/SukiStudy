39a9d04c9594542282ccdf5f7a9d199d2a2883f1
djohnson <mr.demarcus.johnson@gmail.com>
2025-12-24 14:17:25 +0700

    refactor: update transformSubject function to use SubjectType constants for object assignment

--- src/utils/transformSubject.ts
+++ src/utils/transformSubject.ts
@@ -1,13 +1,13 @@
-import { Subject } from '../types'
+import { Subject, SubjectType } from '../types'
 
 export function transformSubject(subject: Subject) {
   let object = subject.object
 
   if (!object && subject.document_url) {
     if (subject.document_url.startsWith('https://www.wanikani.com/radicals')) {
-      object = 'radical'
+      object = SubjectType.RADICAL
     } else if (subject.document_url.startsWith('https://www.wanikani.com/kanji')) {
-      object = 'kanji'
+      object = SubjectType.KANJI
     } else {
       console.error('missing object for subject: ', subject)
     }
